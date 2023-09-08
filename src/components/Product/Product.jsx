f0f22eeaf71625d5386faf07174b142a50fac894
rozyar <rayzor@arboleya.me>
2023-09-07 22:26:58 -0300

    Card product done, page to visualize product created

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
 import { ListProduct as listP } from "./ListProduct";
-
-export default function Product() {
+import './Product.css';
+export default function Product(props) {
   return (
+
+    
     <>
-       <div className="card-ad">
-          <div className="card-ad_content">
-
-
+       <div className="product-card">
+          <div className="product-card-image">
+              <img src={props.image} alt={props.nome} width={240} height={200}/>
           </div>
+          <h2>{props.nome}</h2>
+          <p>R$ {props.preco}</p>
+          <Link to={`vizualizar/produtos/${props.id}`}> <button>Vizualizar</button> </Link>
        </div>
       
     </>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,5 @@
+import Product from '../components/Product/Product'
+import { ListProduct } from '../components/Product/ListProduct'
 import './Home-module.css'
 
 export default function Home(){
@@ -6,6 +8,11 @@ export default function Home(){
             <div className="home-container">
                 <div className='home-title'>
                     <h1>Produtos em destaque</h1>
+                </div>
+                
+                <div className='home-featured-products'>
+                    <Product {...ListProduct[0]}/>
+                    <Product {...ListProduct[1]}/>
                 </div>
             </div>          
         </>
