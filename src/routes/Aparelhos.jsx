6dea7e9809bec46a493e2700eccfd20d49255a2a
rozyar <rayzor@arboleya.me>
2023-09-08 01:02:39 -0300

    aparelhos page done

--- src/routes/Aparelhos.jsx
+++ src/routes/Aparelhos.jsx
@@ -1,7 +1,24 @@
+import { ListProduct } from "../components/Product/ListProduct"
+import Product from "../components/Product/Product"
+import './Aparelhos.css'
+
 export default function Aparelhos(){
     return(
+        
         <>
-            <h1></h1>
+        <div className="aparelhos-container">
+            <div className='aparelhos-title'>
+                <h1>Aparelhos</h1>
+            </div>
+        <div className = "aparelhos">
+
+            {ListProduct.map((p) =>{
+                return(
+                    <Product key={p.id} {...p}/>
+                    )
+                })}
+        </div>
+        </div>
         </>
     )
 }
