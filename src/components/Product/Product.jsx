4e5fd4540caa81fe0360008af5823b78c6699399
rozyar <rayzor@arboleya.me>
2023-09-08 01:03:11 -0300

    Minor changes done in products card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -10,9 +10,9 @@ export default function Product(props) {
           <div className="product-card-image">
               <img src={props.image} alt={props.nome} width={200} height={200}/>
           </div>
-          <h2>{props.nome}</h2>
+          <h3>{props.nome}</h3>
           <p>{props.preco}</p>
-          <Link to={`vizualizar/produtos/${props.id}`}> <button>Vizualizar</button> </Link>
+          <Link to={`/aparelhos/vizualizar/produtos/${props.id}`}> <button>Vizualizar</button> </Link>
        </div>
       
     </>
