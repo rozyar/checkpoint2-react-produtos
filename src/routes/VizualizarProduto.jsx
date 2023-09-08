3f59bcb96f44cb836f73885d11e0a98fe727f128
rozyar <rayzor@arboleya.me>
2023-09-07 23:44:03 -0300

    vizualizar produto done

--- src/routes/VizualizarProduto.jsx
+++ src/routes/VizualizarProduto.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { ListProduct as listP } from '../components/Product/ListProduct';
+import './VisualizarProdutos.css';
 
-export default function VizualizarProduto() {
+export default function VizualizarProdutos() {
+    const {id} = useParams();
+
+    //procurar o produto na lista de produtos pelo id
+    const product = listP.find((p) => p.id === parseInt(id));
     
-    
+    //se o produto não existir, retornar uma mensagem de erro
+    if(!product) return (<h1>Produto não encontrado</h1>)
+
     return (
-        <div>
-            <h1>Visualizar Produto</h1>
-        </div>
+        <>
+
+            <div className="visualizarProduto-container">
+                <div className='visualizarProduto-title'>
+                    <h1>{product.nome}</h1>
+                </div>
+                
+            <div className="product-details">
+                <img src={product.image} alt={product.nome}  width={200} height={200}/>
+                <p>{product.descricao}</p>
+                <p>{product.preco}</p>
+            </div>
+            
+            </div>       
+        </>
     )
 }
