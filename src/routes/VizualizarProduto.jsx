9bee5b16455c3b69a3121ee840f73e02fe750cc3
rozyar <rayzor@arboleya.me>
2023-09-07 22:44:29 -0300

    Aparelhos.jsx and VizualizarProduto.jsx created

---
+++ src/routes/VizualizarProduto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function VizualizarProduto() {
+    
+    
+    return (
+        <div>
+            <h1>Visualizar Produto</h1>
+        </div>
+    )
+}
